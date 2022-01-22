1cb39af9b44a66435813ed09df029267e4611f5d
DongHeon Lee <niche3600@gmail.com>
2022-01-22 20:27:30 +0900

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+'use strict'
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>Hello world</div>
+  )
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
